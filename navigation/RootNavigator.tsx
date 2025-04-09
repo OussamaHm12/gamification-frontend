e5aff17349ae84791731887c1cf5a9ecabb175f9
HMOUDAT OUSSAMA <hamoudatoussama@gmail.com>
2025-04-09 01:09:33 +0100

    ajout de OnboardingScreen

--- navigation/RootNavigator.tsx
+++ navigation/RootNavigator.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigator from './TabNavigator';
+import OnboardingScreen from '../screens/OnboardingScreen';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Onboarding: undefined;
+  Root: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
       <Stack.Screen name="Root" component={TabNavigator} />
     </Stack.Navigator>
   );
